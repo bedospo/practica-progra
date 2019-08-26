177d4a38d377df8790a411c32394e32fa9783cb5
Alumno <Alumno@PC32>
2019-08-26 09:26:41 -0500

    tb

--- practica en clase/practica en clase/Form1.cs
+++ practica en clase/practica en clase/Form1.cs
@@ -16,5 +16,30 @@ namespace practica_en_clase
         {
             InitializeComponent();
         }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="juancho" && textBox2.Text=="123tamarindo")
+            {
+                MessageBox.Show("bienvenido agente juan cho");
+            }
+            else
+            {
+                MessageBox.Show("el nombre de agente o la contraseña es incorrecta");
+                textBox1.Text = "";
+                textBox2.Text="";
+                textBox1.Focus();
+            }
+        }
     }
 }
